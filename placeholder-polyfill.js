eec764d0e182a59a27a23d2a04108537dda174c0
Max Pollack <costello@huskalo.pe>
2013-03-01 16:13:27 -0500

    Placeholder now more accurately updates its position when controls move.

--- placeholder-polyfill.js
+++ placeholder-polyfill.js
@@ -22,25 +22,8 @@
 
 		// Copy the style of the textbox as necessary
 		placeholder.css({
-			position: 'absolute',
-			height: element.height(),
-			width: element.width(),
 			top: position.top,
-			left: position.left,
-			zIndex: 9999,
-			fontFamily: element.css('font-family'),
-			fontSize: element.css('font-size'),
-			marginLeft: element.css('margin-left'),
-			marginRight: element.css('margin-right'),
-			marginTop: element.css('margin-top'),
-			marginBottom: element.css('margin-bottom'),
-			paddingTop: element.css('padding-top'),
-			paddingLeft: element.css('padding-left'),
-			paddingRight: element.css('padding-right'),
-			paddingBottom: element.css('padding-bottom'),
-			lineHeight: element.css('line-height'),
-			color: settings.color,
-			display: 'none'
+			left: position.left
 		});
 	}
 
@@ -74,11 +57,32 @@
 			position;
 
 		placeholder = $('<input/>').
-			val(placeholderText(element));
+			val(placeholderText(element))
+			.css({
+				position: 'absolute',
+				height: element.height(),
+				width: element.width(),
+				zIndex: 9999,
+				fontFamily: element.css('font-family'),
+				fontSize: element.css('font-size'),
+				marginLeft: element.css('margin-left'),
+				marginRight: element.css('margin-right'),
+				marginTop: element.css('margin-top'),
+				marginBottom: element.css('margin-bottom'),
+				paddingTop: element.css('padding-top'),
+				paddingLeft: element.css('padding-left'),
+				paddingRight: element.css('padding-right'),
+				paddingBottom: element.css('padding-bottom'),
+				lineHeight: element.css('line-height'),
+				color: settings.color
+			});
 
 		// Save some configuration on the element
 		element.placeholder = placeholder;
 		placeholder.insertBefore(element);
+
+		// Position it
+		positionPlaceholder(element);
 
 		// Populate the placeholder if the field is empty now
 		if (element.val() === '') {
@@ -98,6 +102,14 @@
 				showPlaceholder(element);
 			}
 		});
+
+		element.parents('form').on('submit', function () {
+			positionPlaceholder(placeholder);
+		});
+
+		setInterval(function () {
+			positionPlaceholder(element);
+		}, 25);
 	}
 
 	// Initializes the polyfill
